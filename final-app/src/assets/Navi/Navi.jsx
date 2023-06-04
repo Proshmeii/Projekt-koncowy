01cf6895d0458695557ada1c709414e5d7212141
Proshmeii <drakowski@o2.pl>
2023-06-04 20:39:31 +0200

    Dodanie nawigacji

--- final-app/src/assets/Navi/Navi.jsx
+++ final-app/src/assets/Navi/Navi.jsx
@@ -1,4 +1,28 @@
-import { useState } from 'react'
-import './assets/Navi.scss'
+import React from 'react'
+import './Navi.scss'
 
+function Navi() {
+    return (
+        <>
+                <header className="layout-header">
+                    <div className="layout-navi">
+                        <div className="navigation-bar">
+                            <div className="v_align">
+                                <a href="/">
+                                    <img className="logo-mobile" src="./images/logo_mobile_img.png" alt="mobile_logo"></img>
+                                    <img className="logo-desktop" src="./images/logo_img.png" alt="desktop_logo"></img>  
+                                </a>
+                            </div>
+                            <div className="navi">
+                                <div className="navi-login-button">Login</div>
+                                <div className="navi-home-button">Home</div>
+                                <div className="navi-about-button">About</div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            
+        </>
+    );
+}
 export default Navi;
--- final-app/src/main.jsx
+++ final-app/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navi from './assets/Navi.jsx'
+import Navi from './assets/Navi/Navi'
 import App from './App.jsx'
 import './App.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
